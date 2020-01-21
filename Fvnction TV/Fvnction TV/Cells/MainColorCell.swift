2bb9fbb1af0fd549a6a4b454b2b3f3b5f95915ad
Alex Linkov <alexey.linkov@gmail.com>
2020-01-21 11:26:34 +0100

    initial

---
+++ Fvnction TV/Fvnction TV/Cells/MainColorCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainColorCell.swift
+//  Fvnction TV
+//
+//  Created by Alex Linkov on 1/21/20.
+//  Copyright © 2020 SDWR. All rights reserved.
+//
+
+import UIKit
+
+class MainColorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
